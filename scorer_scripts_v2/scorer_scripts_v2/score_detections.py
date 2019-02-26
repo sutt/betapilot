b14aa73cc6d3750007a25a906c9bbcb2b1033849
Will Sutton Demo Krak <wsutton17@gmail.com>
2019-02-25 19:00:11 -0500

    more code and 1st notebook

--- scorer_scripts_v2/scorer_scripts_v2/score_detections.py
+++ scorer_scripts_v2/scorer_scripts_v2/score_detections.py
@@ -4,23 +4,45 @@ import argparse
 
 # usage: python score_detections.py -g {ground truth json} -p {prediction json}
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-g","--groundtruthfile", type=str,
-                    help="name of groundtruth file")
-parser.add_argument("-p","--predictionfile", type=str,
-                    help="name of prediction file")
-
-args = parser.parse_args()
-mAP_scorer = mAPScorer()
-
-with open(args.groundtruthfile,'r') as f:
-    GT_data = json.load(f)
-with open(args.predictionfile,'r') as f:
-    pred_data = json.load(f)
+def score(truth_fn, predict_fn):
     
-n_GT = mAP_scorer.countBoxes(GT_data)
-n_Pred = mAP_scorer.countBoxes(pred_data)
+    mAP_scorer = mAPScorer()
 
-coco_score = mAP_scorer.COCO_mAP(GT_data,pred_data)
+    with open(truth_fn,'r') as f:
+        GT_data = json.load(f)
+    with open(predict_fn,'r') as f:
+        pred_data = json.load(f)
+    
+    print len(GT_data), len(pred_data)
+    n_GT = mAP_scorer.countBoxes(GT_data)
+    n_Pred = mAP_scorer.countBoxes(pred_data)
+    
+    coco_score = mAP_scorer.COCO_mAP(GT_data,pred_data)
 
-print("COCO mAP for detector is {}".format(coco_score))
+    print("COCO mAP for detector is {}".format(coco_score))
+
+    return coco_score
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-g","--groundtruthfile", type=str,
+                        help="name of groundtruth file")
+    parser.add_argument("-p","--predictionfile", type=str,
+                        help="name of prediction file")
+
+    args = parser.parse_args()
+    mAP_scorer = mAPScorer()
+
+    with open(args.groundtruthfile,'r') as f:
+        GT_data = json.load(f)
+    with open(args.predictionfile,'r') as f:
+        pred_data = json.load(f)
+        
+    n_GT = mAP_scorer.countBoxes(GT_data)
+    n_Pred = mAP_scorer.countBoxes(pred_data)
+    print len(GT_data.keys())
+    print len(GT_data.keys())
+    coco_score = mAP_scorer.COCO_mAP(GT_data,pred_data)
+
+    print("COCO mAP for detector is {}".format(coco_score))
